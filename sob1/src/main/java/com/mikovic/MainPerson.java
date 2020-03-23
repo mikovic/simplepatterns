4ec5b3ebf99aed2888a098094329d93efdd21eca
Konstantin Minakov <mikovic@yandex.ru>
2020-03-23 17:30:42 +0300

    1-sobes(подправил обращение через интерфейс)

--- sob1/src/main/java/com/mikovic/MainPerson.java
+++ sob1/src/main/java/com/mikovic/MainPerson.java
@@ -5,6 +5,7 @@ import com.mikovic.entities.mistake.LightWeightCar;
 import com.mikovic.entities.mistake.Lorry;
 import com.mikovic.entities.patternBuilder.Person;
 import com.mikovic.entities.poliform.Circle;
+import com.mikovic.entities.poliform.Sqrt;
 import com.mikovic.entities.poliform.Square;
 import com.mikovic.entities.poliform.Triangle;
 
@@ -36,6 +37,13 @@ public class MainPerson {
         Square square = new Square(10.55);
         square.draw();
         square.inform();
+        System.out.println("3 полиформизм____________________________________________");
+        Sqrt circle1 = new Circle(11);
+        System.out.printf("Площадь  =%.2f \n ", circle1.getSqrt());
+        Sqrt triangle1 = new Triangle(12.256, 14.2345);
+        System.out.printf("Площадь  =%.2f \n ", triangle1.getSqrt());
+        Sqrt square1 = new Square(9.55);
+        System.out.printf("Площадь  =%.2f \n ", square1.getSqrt());
 
 
     }
